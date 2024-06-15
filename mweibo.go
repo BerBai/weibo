bdc360fc5cad702e3142f6ff0b3d6ef2aa2b59b3
ber <bai5775@outlook.com>
2024-06-16 07:16:55 +0800

    feat：全部微博带互动，需cookie

--- mweibo.go
+++ mweibo.go
@@ -92,9 +92,50 @@ func (c *Client) FetchCMblogLongText(mblog *CMblog) error {
 	}
 }
 
-// 需要cookie
+// 获取互动信息，需要cookie
 func (c *Client) GetCMblogs(userid string, page int, longtext bool) ([]*CMblog, error) {
 	blogUrl := fmt.Sprintf("https://m.weibo.cn/api/container/getIndex?containerid=230869%s_-_comment&page_type=03&page=%d", userid, page)
+	body := &CMblogBody{}
+	if err := c.getJSON(blogUrl, body); err != nil {
+		return nil, err
+	} else if body.Ok != 1 {
+		return nil, fmt.Errorf("body not ok")
+	}
+	var mblogs []*CMblog
+	for _, card := range body.Data.Cards {
+
+		if card.CardType == 11 {
+			if longtext {
+				if err := c.FetchCMblogLongText(&card.CardGroup[0].Mblog); err != nil {
+					return nil, err
+				}
+				if card.CardGroup[0].Mblog.Retweeted != nil {
+					if err := c.FetchCMblogLongText(card.CardGroup[0].Mblog.Retweeted); err != nil {
+						return nil, err
+					}
+				}
+			}
+			mblogs = append(mblogs, &card.CardGroup[0].Mblog)
+		} else if card.CardType == 9 {
+			if longtext {
+				if err := c.FetchCMblogLongText(&card.Mblog); err != nil {
+					return nil, err
+				}
+				if card.Mblog.Retweeted != nil {
+					if err := c.FetchCMblogLongText(card.Mblog.Retweeted); err != nil {
+						return nil, err
+					}
+				}
+			}
+			mblogs = append(mblogs, &card.Mblog)
+		}
+	}
+	return mblogs, nil
+}
+
+// 手机端api，获取全部微博，需要cookie
+func (c *Client) GetMMblogs(userid string, page int, longtext bool) ([]*CMblog, error) {
+	blogUrl := fmt.Sprintf("https://m.weibo.cn/api/container/getIndex?containerid=230413%s_-_WEIBO_SECOND_PROFILE_WEIBO&page_type=01&page=%d", userid, page)
 	body := &CMblogBody{}
 	if err := c.getJSON(blogUrl, body); err != nil {
 		return nil, err
