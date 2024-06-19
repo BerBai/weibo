f81d8dfb9375ac21de9d9d0d603f7fa229010e1e
ber <bai5775@outlook.com>
2024-06-19 21:35:51 +0800

    fix: 过滤无效文本

--- mweibo.go
+++ mweibo.go
@@ -105,11 +105,11 @@ func (c *Client) GetCMblogs(userid string, page int, longtext bool) ([]*CMblog, 
 	}
 	var mblogs []*CMblog
 	for _, card := range body.Data.Cards {
-		if card.ShowType == 0 {
-			continue
-		}
+		//if card.ShowType == 0 {
+		//	continue
+		//}
 		if card.CardType == 11 {
-			if longtext {
+			if longtext && len(card.CardGroup) > 0 {
 				if err := c.FetchCMblogLongText(&card.CardGroup[0].Mblog); err != nil {
 					return nil, err
 				}
@@ -148,11 +148,11 @@ func (c *Client) GetMMblogs(userid string, page int, longtext bool) ([]*CMblog, 
 	}
 	var mblogs []*CMblog
 	for _, card := range body.Data.Cards {
-		if card.ShowType == 0 {
-			continue
-		}
+		//if card.ShowType == 0 {
+		//	continue
+		//}
 		if card.CardType == 11 {
-			if longtext {
+			if longtext && len(card.CardGroup) > 0 {
 				if err := c.FetchCMblogLongText(&card.CardGroup[0].Mblog); err != nil {
 					return nil, err
 				}
